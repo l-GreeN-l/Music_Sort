a5014cca7e80f2b866f5e5dc4c290218c441c96f
Z_green_Z <valdisvan@mail.ru>
2022-02-07 17:34:32 +0300

    Update sort_methods.py

--- Music_Sort/sort_methods.py
+++ Music_Sort/sort_methods.py
@@ -6,33 +6,26 @@ from collections import OrderedDict
 from methods import get_files
 
 
-path = 'D:\Downloads\Music_VK'
+path = 'D:\Downloads\Music'
 dictination = 'D:\Downloads'
-
 
 def get_group(filename):
     buf = os.path.basename(filename).split(' - ')
     return buf[0].upper()
 
-
-
 def sort_in_folders(path):
     os.chdir(path)
 
     files = get_files()
-
-
-
     add_names = []
     buffer = []
     catalog = []
     folder = []
 
-
-
     for sel_file in files:
         sel_group = get_group(sel_file.name)
         # print(sel_group)
+        
         # Есть ли группа уже в сортированных
         #  Удалять файлы из files во время прохода мы не можем - нарушит итерацию
         if sel_group in add_names:
@@ -73,35 +66,20 @@ def sort_in_folders(path):
     # print('buffer: ', buffer)
     # print('len catalog: ', len(catalog))
     print('Catalog building !')
-
-
-
     return catalog
-
-
-
 
 def create_folder(name):
     try:
         os.mkdir(name)
-
-
-
     except FileExistsError as erF:
         print('Директория уже есть')
     pass
 
-
-
-
-
 #  Перемещение файлов и создание каталога
 def transfer(catalog, dist):
-
     catalog_name = 'Sorted_music'
     if len(catalog) !=0:
         os.chdir(dist)
-
         create_folder(catalog_name)
         os.chdir(catalog_name)
         koren = os.getcwd()
@@ -123,8 +101,6 @@ def transfer(catalog, dist):
                 print(elem)
                 shutil.move(elem, os.getcwd())
 
-
-
     #     for file in delFiles:
     #         shutil.move(file, distination_del + os.path.basename(file))
     #         pass
@@ -135,19 +111,8 @@ def transfer(catalog, dist):
     #
     # pass
 
-
-
-
-
-
-
-
-
-
 catalog  = sort_in_folders(path)
 transfer(catalog, dictination)
-
-
 
 # for elem in catalog:
 #     if type(elem) == type([]):
